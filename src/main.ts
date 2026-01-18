ad00aa0ed676c3cfbb199f2827640d917a199fd0
rich <progamedesigners@gmail.com>
2026-01-17 18:54:01 -0600

    Fix build: import Phaser via modules

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
-const config = {
+import Phaser from "phaser";
+
+const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   backgroundColor: "#000000",
   scene: {
-    create: function () {
+    create: function() {
       this.add.text(100, 100, "Phaser is alive", {
         font: "32px Arial",
-        fill: "#ffffff"
+        color: "#ffffff",
       });
-    }
-  }
+    },
+  },
 };
 
 new Phaser.Game(config);
